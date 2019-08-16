4248159cbd894fd07976989f05a59dc8f57cd749
Christian Cury <contato.mikesantos@gmail.com>
2019-08-16 18:14:55 -0300

    feat: Structure

---
+++ src/pages/Status/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Header from "../../components/Header";
+
+export default class Status extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    );
+  }
+}
